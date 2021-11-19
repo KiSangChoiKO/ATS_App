ff254702a57fcca0957169d3be89b968314eae97
99kenny <helloiamkenny@gmail.com>
2021-11-20 07:55:03 +0900

    link/memberDB

--- app/src/main/java/com/example/ats_app/LoginActivity.java
+++ app/src/main/java/com/example/ats_app/LoginActivity.java
@@ -1,19 +1,54 @@
 package com.example.ats_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText editId, editPw;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
     }
 
-    public void login(View view){
+    public void login(View view) {
 
+        editId=(EditText)findViewById(R.id.editId);
+        editPw=(EditText)findViewById(R.id.editPw);
+
+        String id = editId.getText().toString();
+        String pw = editPw.getText().toString();
+
+        firebaseAuth = firebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(id, pw)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {//성공했을때
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {//실패했을때
+                            try{
+                                task.getResult();
+                            }catch(Exception e){
+                                e.printStackTrace();
+                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
     }
+
 }
--- app/src/main/java/com/example/ats_app/LogupActivity.java
+++ app/src/main/java/com/example/ats_app/LogupActivity.java
@@ -1,19 +1,142 @@
 package com.example.ats_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LogupActivity extends AppCompatActivity {
+
+    EditText editId,editEmail,editName, editPw, editPwConfirm;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logup);
+        setContentView(R.layout.logup);
     }
 
-    public void logup(View view){
 
+    public void logup(View view) {
+
+        editEmail=(EditText)findViewById(R.id.editEmail);
+        editName=(EditText)findViewById(R.id.editName);
+        editId=(EditText)findViewById(R.id.editId);
+        editPw=(EditText)findViewById(R.id.editPw);
+        editPwConfirm=(EditText)findViewById(R.id.editPwConfirm);
+
+        String name = editName.getText().toString();
+        String email = editEmail.getText().toString();
+        String id = editId.getText().toString();
+        String pw = editPw.getText().toString();
+        String pwConfirm = editPwConfirm.getText().toString();
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+        //나중에 추가
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        //
+
+
+        if(pw.equals(pwConfirm) && isValidPasswd() && isValidEmail() && isValidName()) {
+
+            firebaseAuth.createUserWithEmailAndPassword(email, pw).addOnCompleteListener(LogupActivity.this, new OnCompleteListener<AuthResult>() {
+
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        //나중에추가
+                        Log.d("Sign up", "DocumentSnapshot added on auth");
+
+                        user.put("email",email);
+                        user.put("id",id);
+                        user.put("name",name);
+                        user.put("pw",pw);
+                        user.put("type",2);
+
+                        //나중에 추가
+                        db.collection("member").document(email)
+                                .set(user)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d("Sign_up_db", "DocumentSnapshot successfully written!");
+                                        Toast.makeText(LogupActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                                        finish();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w("Sign_up_db", "Error writing document", e);
+                                    }
+                                });
+                        //
+                    }
+                    else{
+                        try{
+                            task.getResult();
+                        }catch (Exception e){
+                            e.printStackTrace();
+                            Log.w("Sign_up_Auth", "Error adding document", e);
+                            Toast.makeText(LogupActivity.this, "오류 발생", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }
+            });
+        }
+        else{
+            Log.v("Sign_up_Valid","Validity does not match");
+            Toast.makeText(getBaseContext(),"회원가입에 실패하였습니다. (비밀번호 일치여부, 이메일 공백 여부 확인)",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isValidEmail() {
+        if (editId.getText().toString().equals("")) {
+            // 이메일 공백
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private boolean isValidName() {
+
+        if (editId.getText().toString().equals("")) {
+            // 이메일 공백
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    // 비밀번호 유효성 검사
+    private boolean isValidPasswd() {
+        if (editPw.getText().toString().equals("")) {
+            // 비밀번호 공백
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
